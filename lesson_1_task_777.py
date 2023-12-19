2c9fa58e31874944145b30bafa4ee1f595171763
Ingvar <scorpio118@yandex.ru>
2023-12-19 15:26:01 +0300

    lesson 1

---
+++ lesson_1_task_777.py
@@ -0,0 +1,60 @@
+def func1():
+    print("1" , end=" ")
+    
+
+def func2():
+    print("2" , end=" ")
+
+def func3():
+    print("3" , end=" ")
+
+def func4():
+    print("4" , end=" ")
+
+
+def func5():
+    print("5" , end=" ")
+
+
+def func6():
+    print("6" , end=" ")
+
+
+def func7():
+    print("7" , end=" ")
+
+
+def func8():
+    print("8" , end=" ")
+
+
+def func9():
+    print("9" , end=" ")
+
+
+def func10():
+    print("0" , end=" ")
+
+func1()
+func2()
+func3()
+func4()
+func5()
+func6()
+func7()
+func8()
+func9()
+func10()
+
+print("      ")
+func8()
+func8()
+func10()
+func10()
+func5()
+func5()
+func5()
+func3()
+func5()
+func3()
+func5()
